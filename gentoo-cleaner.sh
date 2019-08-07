aad44fff01a68bb7c01dd86f2c36de07b10a6da5
leg0las-devel <elfleg0las88@gmail.com>
2019-08-08 01:55:54 +0300

    Removed x86 support, updated PATH (17.1 profile)

--- gentoo-cleaner.sh
+++ gentoo-cleaner.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-# gentoo-cleaner-0.0.1.2-r2
-PWD_DIR="$(whereis gentoo-cleaner.sh | awk '{print $2}' | sed "s/\/gentoo-cleaner.sh//g")"
-
+# gentoo-cleaner-0.0.2
+CONF_DIR="/etc/gentoo-cleaner"
 # User exceptions:
-EXCLUDE_CONF="$PWD_DIR/user-exeptions.conf"
+EXCLUDE_CONF="$CONF_DIR/user-exeptions.conf"
 
 # Paths for find system files
-if [ "$(uname -m)" != "x86_64" ]
-then
-INCLUDE_PATH="/bin/ /etc/ /lib/ /opt/ /sbin/ /usr/"
-else
-INCLUDE_PATH="/bin/ /etc/ /lib32/ /lib64/ /opt/ /sbin/ /usr/"
-fi
+INCLUDE_PATH="/bin/ /etc/ /lib/ /lib64/ /opt/ /sbin/ /usr/"
 
 # Exclude paths (system)
 EXCLUDE_PATH="
 /etc/local.d
 /etc/portage
 /etc/runlevels
+/lib/modules
 /usr/local
 /usr/portage
 /usr/share/mime
 /usr/src
 /usr/tmp
 "
-if [ "$(uname -m)" != "x86_64" ]
-then
-    EXCLUDE_PATH+="/lib/modules"
-else
-    EXCLUDE_PATH+="/lib64/modules"
-fi
 
 # Exceptions paths (users)
 USER_EXCLUDE_PATH="$(cat $EXCLUDE_CONF | grep "^/")"
@@ -40,11 +29,6 @@ TMP_DIR="/tmp/garbage"
 LOG_DIR="/var/log"
 TMP_PACKAGE_FILES="$TMP_DIR/package_files"
 TMP_PACKAGE_FILES_SORT="$TMP_DIR/package_files_sort"
-if [ "$(uname -m)" = "x86_64" ]
-then
-TMP_PACKAGE_FILES_SORT_LIB64="$TMP_DIR/package_files_sort_lib64"
-TMP_PACKAGE_FILES_SORT_LIB64_USR_LIB64="$TMP_DIR/package_files_sort_lib64_usr_lib64"
-fi
 TMP_PACKAGE_FILES_RESULT="$TMP_DIR/package_files_result"
 TMP_SYSTEM_FILES="$TMP_DIR/system_files"
 TMP_SYSTEM_FILES_SORT="$TMP_DIR/system_files_sort"
@@ -61,16 +45,7 @@ mkdir "${TMP_DIR}"
 
 # Creating filelist for packages
 find /var/db/pkg/ -name CONTENTS -exec cat {} \; >> "$TMP_PACKAGE_FILES"
-cat "$TMP_PACKAGE_FILES" | awk '{print $2}' | grep -v "^/usr/src" | sort -u > "$TMP_PACKAGE_FILES_SORT"
-# Moving /lib -> /lib64 and /usr/lib -> /usr/lib64 for x86_64, sorting
-if [ "$(uname -m)" = "x86_64" ]
-then
-cat "$TMP_PACKAGE_FILES_SORT" | sed "s/^\/lib\//\/lib64\//g" > $TMP_PACKAGE_FILES_SORT_LIB64
-cat "$TMP_PACKAGE_FILES_SORT_LIB64" | sed "s/^\/usr\/lib\//\/usr\/lib64\//g" > $TMP_PACKAGE_FILES_SORT_LIB64_USR_LIB64
-cat "$TMP_PACKAGE_FILES_SORT_LIB64_USR_LIB64" | sort -u > "$TMP_PACKAGE_FILES_RESULT"
-else
-cp "$TMP_PACKAGE_FILES_SORT" "$TMP_PACKAGE_FILES_RESULT"
-fi
+cat "$TMP_PACKAGE_FILES" | awk '{print $2}' | grep -v "^/usr/src" | sort -u > "$TMP_PACKAGE_FILES_RESULT"
 
 # Creating system filelist according to $INCLUDE_PATH
 find $INCLUDE_PATH > $TMP_SYSTEM_FILES
